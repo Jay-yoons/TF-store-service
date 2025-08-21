b68b91a9e4d1d1d705aba6694b2c87e6df72269e
ddongj333 <kdj1289@gmail.com>
2025-08-21 15:22:15 +0900

    jwt ê인증제거(필요시나중에on가능)

--- src/main/java/com/example/store/service/config/SecurityConfig.java
+++ src/main/java/com/example/store/service/config/SecurityConfig.java
@@ -3,17 +3,10 @@ package com.example.store.service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri("https://cognito-idp.ap-northeast-2.amazonaws.com/ap-northeast-2_bdkXgjghs/.well-known/jwks.json").build();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,9 +14,8 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/actuator/health", "/actuator/info", "/api/stores/**", "/api/reviews/stores/**", "/api/reviews/{id}").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth -> oauth.jwt());
+                .anyRequest().permitAll()  // 모든 요청 허용
+            );
         return http.build();
     }
 }
