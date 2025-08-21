2fb8495f44da1463d3b2aa40ba4ab11e1c8d3481
KHP-SOLDESK1-03\SD1-03 <asanyong1@naver.com>
2025-08-21 11:28:15 +0900

    0821 스토어 코드 수정

--- src/main/java/com/example/store/service/config/SecurityConfig.java
+++ src/main/java/com/example/store/service/config/SecurityConfig.java
@@ -3,10 +3,17 @@ package com.example.store.service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://cognito-idp.ap-northeast-2.amazonaws.com/ap-northeast-2_bdkXgjghs/.well-known/jwks.json").build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
